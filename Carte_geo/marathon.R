8b0cbd033093e435e3c688d2e2bda9360b6f413e
raissa-23 <60789703+raissa-23@users.noreply.github.com>
2022-03-10 16:15:04 +0100

    update carte géo

--- Carte_geo/marathon.R
+++ Carte_geo/marathon.R
@@ -1,19 +1,54 @@
+
+#La carte de l'europe
+
 #install.packages("leaflet")
+library(dplyr)
 
-library(leaflet)
+#pré-traitement des données
+total_vente <- read.table("pays_ventes.csv") 
+tot_vente <- total_vente %>% 
+  filter(!is.na(total_vente$countries))
+coord <- read_excel("Coordonees.xlsx")
+coo <- coord %>% 
+  filter(coord$Countries!="Andorre")
+vente_coord <- cbind(coo,tot_vente) %>% 
+  select(-countries) %>% 
+  rename(countries=Countries)
+
+View(vente_coord)
+#str(vente_coord)
 
 
-library(leaflet)
-m <- leaflet() %>%
-  fitBounds(-20,65,22,39) %>% 
-  addProviderTiles(providers$CartoDB.Positron)
- addTiles()%>% 
-addMarkers(lng=2.3522219, lat=48.856614, popup="Nombre de commande")
-m
 
-#Pour le zoom
+#Pour generer la carte
+leaflet(vente_coord,options = leafletOptions(minZoom = 0, maxZoom = 18)) %>%
+  fitBounds(-20,65,22,39) %>%
+  addTiles()%>%
+  
+  #pour le design de la carte on a :
+  addProviderTiles(providers$Esri.NatGeoWorldMap)%>%
+  #ou
+  #addProviderTiles("Stamen.TonerHybrid")%>%
+  #addProviderTiles(providers$CartoDB.Positron)%>%
+  
+  
+  #pouvoir afficher un message ? l'atitude et longitude design?
+  #addMarkers(lng=2.209667, lat=46.232193, popup="Nous sommes les chmpions du maraton Web")
+  
+  #recuperation des données pour les afficher dans la carte
+  #On peut Remplacer "countries" par l'information que l'on veut afficher dans la carte
+  addMarkers(~long, ~lat, popup = ~as.character(total_ventes_pays2021), label = ~as.character(total_ventes_pays2021))
 
-#m <- leaflet() %>%
- # addTiles() %>%  # Add default OpenStreetMap map tiles
- # addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
-m  # Print the map
+#m
+
+
+
+
+
+
+
+
+
+
+
+
